560b6ab557b7918180803d5a236ed90737838693
sudosubin <sudosubin@gmail.com>
2025-04-12 00:03:48 +0900

    feat: update to use CFLAGS for pillow-heif

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       inherit (nixpkgs.lib) genAttrs platforms;
       forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs { inherit system; }));
@@ -15,8 +16,14 @@
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            libheif
             uv
           ];
+
+          shellHook = ''
+            # pillow-heif: fix libheif.h not found
+            export CFLAGS="$(echo $NIX_CFLAGS_COMPILE | sed -e "s/-isystem /-I/g") -Wno-error=deprecated-ofast"
+          '';
         };
       });
     };
